a8cb346a674070a606e8e67d79a2c7b580a85627
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2021-11-15 18:42:16 -0300

    Agregando funciones

--- level3/Ejercicio1.java
+++ level3/Ejercicio1.java
@@ -1,27 +1,29 @@
 package level3;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Ejercicio1 {
 
-    private static String nulo = null;
     public static void main(String[] args) {
+        filtrar(lista());
+    }
 
+    public static ArrayList<String> lista (){
         ArrayList<String> palabras = new ArrayList<>();
         palabras.add("Hola");
         palabras.add(" ");
         palabras.add(null);
         palabras.add("Informatorio");
 
+        return palabras;
+    }
 
+    public static void filtrar(ArrayList<String> palabras){
         List<String> nuevasPalabras = palabras.stream()
-                .filter(Objects::nonNull)
-                .filter(n -> !Objects.equals(n, " "))
-                .collect(Collectors.toList());
+            .filter(Objects::nonNull)
+            .filter(n -> !Objects.equals(n, " "))
+            .collect(Collectors.toList());
 
         nuevasPalabras.forEach(System.out::println);
     }
