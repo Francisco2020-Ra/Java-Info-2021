1df3b29edc8033e83ae1f3d8387d673842ce66c3
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2021-11-15 21:38:39 -0300

    Potencia de un numero con Stream

---
+++ level3/Ejercicio2.java
@@ -0,0 +1,30 @@
+package level3;
+/*
+Dada una lista que contiene números enteros,
+se deberá generar otra lista que contendrá
+el resultado de aplicar la operación de potencia a 2.
+*/
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        potenciaciacion(
+                numeroEnteros()
+        );
+    }
+
+    public static List<Integer> numeroEnteros(){
+        return List.of(1, 2, 3, 4, 5);
+    }
+
+    public static void potenciaciacion(List<Integer> numeroEnteros){
+        List<Integer> potencia = numeroEnteros.stream()
+                .map(n -> n*n)
+                //.peek(n -> System.out.print(n + ", " ))
+                .collect(Collectors.toList());
+
+       potencia.forEach(n -> System.out.print(n +", "));
+    }
+}
