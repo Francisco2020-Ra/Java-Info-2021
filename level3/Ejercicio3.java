d2d9ca95569182b603f32e2e8b468c7797cf9bec
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2021-11-16 14:55:25 -0300

    Utilizando primero map y luego filter con startsWitch

---
+++ level3/Ejercicio3.java
@@ -0,0 +1,37 @@
+package level3;
+
+/*
+Se dispone de una lista de Strings,de la cual saber la cantidad de valores
+que comiencen con la letra B (tanto mayúscula como minúscula, en ese
+caso decimos que será ignoreCase).
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        imprimirCantidad(
+            crearLista()
+        );
+    }
+
+    public static List<String> crearLista(){
+        return List.of("Batman", "Aquaman", "Flash", "Batgirl", "Wonder Woman", "brainiac");
+    }
+
+    public static void imprimirCantidad(List<String> lista){
+
+        List<String> palabras =lista.stream()
+                .map(String::toLowerCase)
+                //.peek(n -> System.out.println(n))
+                .filter(palabra -> palabra.startsWith("b"))
+                .collect(Collectors.toList());
+
+        System.out.println(palabras.size());
+
+    }
+}
